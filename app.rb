10c88efb3a5016116feb56ee5ad2fc6b9c5c6cd3
cesare camurani <cesare.camurani@gmail.com>
2018-11-13 17:50:06 +0000

    Hit Points

--- app.rb
+++ app.rb
@@ -12,12 +12,14 @@ class Battle < Sinatra::Base
   post '/names' do
     session[:your_name] = params[:your_name]
     session[:enemy_name] = params[:enemy_name]
+    session[:enemy_hit_points] = params[:enemy_hit_points]
     redirect '/play'
   end
 
   get '/play' do
     @your_name = session[:your_name]
     @enemy_name = session[:enemy_name]
+    @enemy_hit_points = session[:enemy_hit_points]
     erb(:play)
   end
 
--- spec/features/feature_tests_spec.rb
+++ spec/features/feature_tests_spec.rb
@@ -1,17 +1,13 @@
 
-# feature 'Testing infrastructure' do
-#   scenario 'Can run app and check page content' do
-#     visit('/')
-#     expect(page).to have_content 'Testing infrastructure working!'
-#   end
-# end
-
 feature 'Testing user names' do
   scenario 'Users can fill in their names in a form' do
     visit('/')
     fill_in('your_name', with: 'Cesare')
     fill_in('enemy_name', with: 'Satan')
+    fill_in('enemy_hit_points', with: '100pts')
     click_button("Submit")
-    expect(page).to have_content("Cesare", "Satan")
+    expect(page).to have_content("Cesare")
+    expect(page).to have_content("Satan")
+    expect(page).to have_content("100pts")
   end
 end
